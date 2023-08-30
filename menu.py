c8e9bd0ae3a417e269b5327d4385e472f849eda2
yenikscode <126620103+yenikscode@users.noreply.github.com>
2023-08-30 11:36:39 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,6 +9,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 
 st.write("Hello World")
